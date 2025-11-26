395b1fea84bb35212acf6b885a4d2560fcf2a140
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-11-26 22:39:26 +0530

    add bio section

--- types/index.tsx
+++ types/index.tsx
@@ -1,6 +1,7 @@
 
-import { Github, Linkedin, MailIcon, Twitter } from "lucide-react";
-
+import { Code, DotIcon, Github,  Linkedin, MailIcon, Map, Twitter } from "lucide-react";
+import { GoDotFill } from "react-icons/go";
+import { SiPeerlist } from "react-icons/si";
 export interface ChildrenProps {
   children: React.ReactNode;
 }
@@ -47,9 +48,35 @@ export const SocialMedia: MediaLink[] = [
     lablename: "LinkedIn",
     link:"https://www.linkedin.com/in/isuzwal-9719ba289/"
   },
-  {
-    icon: <MailIcon   className="size-5"/>,
-    lablename: "Gmail",
-    link:"mailto:isuzwal@gamil.com"
-  },
+    {
+      icon:<SiPeerlist className="size-5" />,
+      lablename:"PeerList",
+      link:"https://peerlist.io/lufffy"
+
+    }
 ];
+
+
+interface BIOProps{
+  icons:React.ReactNode;
+  data:string;
+}
+export const ShortBio:BIOProps[]=[
+   {
+    icons:<Code  className="size-5" />,
+    data:"Full Stack developer"
+   },
+   {
+    icons:<Map  className="size-5" />,
+    data:"Nepal,Np"
+   },
+   {
+  icons:<MailIcon  className="size-5" />,
+  data:"ujjwalgaihre0@gmail.com"
+   },
+   {
+   icons:<GoDotFill className="size-5 text-green-400  drop-shadow-[0_0_6px_#22c55e] " />,
+   data:" Available for Hire"
+   }
+
+  ]
