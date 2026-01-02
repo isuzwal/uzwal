0e0f10d5c03ebfb439b83cfd3ff52d03ba9dba71
isuzwal <ujjwalgaihre0@gmail.com>
2026-01-02 23:52:36 +0530

    working on the something

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SkillsView } from '@/components/ui/skills-view';
 
 export default function Home() {
   return (
-    <div className="font-medium font-mono min-h-screen">
+    <div className=" font-mono min-h-screen">
       <HeroSection />
       <SkillsView />
       <Experiences />
--- types/index.tsx
+++ types/index.tsx
@@ -188,7 +188,7 @@ export const Skills: SkillsProps[] = [
         />
         <path
           fill="currentColor"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M5.563 17.146c1.924.702 3.936-.094 4.959-1.647c.2-.305.53-.498.896-.498h.255c-.94 2.437-3.72 3.856-6.363 2.905C3.287 17.178 2 15.179 2 13.029V10.8a5.01 5.01 0 0 1 5.486-4.981c2.593.243 4.521 2.523 4.521 5.127V12.6H2.801v.444c0 1.802 1.069 3.485 2.762 4.102m1.44-10.55a4.21 4.21 0 0 0-4.202 4.202V11.8h8.406v-1a4.21 4.21 0 0 0-4.203-4.204"
           clipRule="evenodd"
         />
@@ -690,7 +690,7 @@ export const ProjectsData: ProjectProps[] = [
 
 export const MyPassions: Passion[] = [
   {
-    name: 'Love to Play Football',
+    name: 'Play Football',
   },
   {
     name: 'Watching Animes',
